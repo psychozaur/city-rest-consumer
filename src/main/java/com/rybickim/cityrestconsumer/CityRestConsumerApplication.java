c16746923896150928061b5914d7c8daf52f6a90
Marcin Rybicki <m.rybicki1112@gmail.com>
2019-05-18 14:28:05 +0200

    [init] - gitignore and initial commit

---
+++ src/main/java/com/rybickim/cityrestconsumer/CityRestConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.rybickim.cityrestconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityRestConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CityRestConsumerApplication.class, args);
+    }
+
+}
